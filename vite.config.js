0e35647673cc943df96baaf83a2443a874d7fb26
wlkqyang-star <wlkqyang@gmail.com>
2025-09-07 23:12:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/format-converter/',
+  base: '/1223/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
